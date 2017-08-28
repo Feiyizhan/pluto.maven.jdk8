e33cc10a03d97f1fe836dedf35d12cb994034f7d
LAPA\A4YL9ZZ <pxu3@mmm.com>
2017-08-28 10:17:51 +0800

    获取运行时的JDK版本

---
+++ src/main/java/com/pluto/maven/jdk8/chat2/GetProperty.java
@@ -0,0 +1,18 @@
+package com.pluto.maven.jdk8.chat2;
+
+/**
+ * @author Pluto Xu a4yl9zz
+ *
+ */
+public class GetProperty {
+
+    /**
+     * @author Pluto Xu a4yl9zz
+     * @param args
+     */
+    public static void main(String[] args) {
+        //获取JDK版本
+        System.out.println(System.getProperty("java.specification.version"));
+    }
+
+}
