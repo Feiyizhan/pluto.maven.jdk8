58cd6608b98a00847cae12ae8a60d73b866ba6c7
LAPA\A4YL9ZZ <pxu3@mmm.com>
2017-08-28 10:17:23 +0800

    测试输出本机系统的path路径系统参数

---
+++ src/main/java/com/pluto/maven/jdk8/chat2/GetEnv.java
@@ -0,0 +1,17 @@
+package com.pluto.maven.jdk8.chat2;
+
+/**
+ * @author Pluto Xu a4yl9zz
+ *
+ */
+public class GetEnv {
+
+    /**
+     * @author Pluto Xu a4yl9zz
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("System.getenv(\"PATH\"="+System.getenv("PATH"));
+    }
+
+}
