723ee462fa59ed2a2721f657d7ec8da1e62b9552
xuml <xuml@abccapital.cn>
2019-05-22 18:36:07 +0800

    优化随机日期生成

--- src/test/java/com/pluto/maven/jdk8/stream/BaseTests.java
+++ src/test/java/com/pluto/maven/jdk8/stream/BaseTests.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
@@ -27,17 +28,19 @@ public class BaseTests {
     protected List<UserData> dataList100 = new ArrayList<>();
     
     protected List<UserData> dataList10 = new ArrayList<>();
+    
+    protected List<Integer> statusList = Arrays.asList(10,20,30);
 
     @Before
     public void initDate() {
         for(int i=0;i<100000;i++) {
             UserData itemData = new UserData();
             itemData.setName(RandomStringUtils.random(3,"abc"));
-            itemData.setStatus(RandomUtils.nextInt(1, 3)*10);
+            itemData.setStatus(RandomUtils.nextInt(1, 4)*10);
             itemData.setDate(LocalDateTime.of(
-                RandomUtils.nextInt(2016, 2019), 
-                RandomUtils.nextInt(1, 12), 
-                RandomUtils.nextInt(1, 28), 
+                RandomUtils.nextInt(2016, 2020), 
+                RandomUtils.nextInt(1, 13), 
+                RandomUtils.nextInt(1, 29), 
                 RandomUtils.nextInt(1,24), 
                 RandomUtils.nextInt(1,60)));
             itemData.setAmount1(BigDecimal.valueOf(RandomUtils.nextLong(1, 10000)).divide(new BigDecimal("100"), 2,RoundingMode.HALF_UP));
